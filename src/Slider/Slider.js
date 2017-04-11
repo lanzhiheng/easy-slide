d028f079531d5745c2b8772326feea1a0a7315fd
lanzhiheng <hengrj@126.com>
2017-04-11 15:39:25 +0800

    move event binding to component file

--- src/Slider/Slider.js
+++ src/Slider/Slider.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import './Slider.css';
 
+// http://stackoverflow.com/questions/5898656/test-if-an-element-contains-a-class
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+}
+
 class Slider extends Component {
   constructor() {
     super();
@@ -30,5 +35,78 @@ class Slider extends Component {
   }
 }
 
+export const loadTouchEvent = () => {
 
-export default Slider
+  let currentX, prevX, direction, sliderButton;
+
+  sliderButton = document.getElementsByClassName('slider-button')[0];
+
+  let sliderButtonWidth = sliderButton.offsetWidth;
+
+  // touchend 的事件回调函数
+  let touchEndFunction = (e) => {
+    let currentComponent = e.target;
+
+    if (!hasClass(currentComponent, 'slider-content')) return;
+
+    if (direction === 'left') {
+      currentComponent.style.left = `-${sliderButtonWidth}px`;
+    } else if (direction === 'right') {
+      currentComponent.style.left = '0px';
+    }
+
+    prevX = undefined;
+    currentX = undefined;
+
+    // 取消过渡效果
+    currentComponent.style.transition = '0.3s all ease-in';
+
+
+    // 每次这个事件调用之后移除对应的事件，免得内存泄漏
+    currentComponent.removeEventListener('touchend', touchEndFunction);
+    currentComponent.touchendExist = undefined;
+  }
+
+  document.body.addEventListener('touchmove', e => {
+    let currentComponent = e.target;
+
+
+    // 判断是否是目标的对象
+    if (!hasClass(currentComponent, 'slider-content')) return;
+
+    currentX = e.touches[0].pageX;
+    currentComponent.style.transition = '';
+
+    let currentComponentOffset = currentComponent.offsetLeft;
+
+    if (prevX) {
+      if (currentComponentOffset <= 0 && currentComponentOffset >= -sliderButtonWidth) {
+
+        if (currentX < prevX) {
+          direction = 'left';
+          currentComponent.style.left = (currentComponentOffset - 1) + 'px';
+
+        } else {
+          direction = 'right';
+          currentComponent.style.left = (currentComponentOffset + 1) + 'px';
+        }
+      }
+    }
+
+    prevX = currentX;
+
+    // 判断是否绑定了`touchend`事件，适用组件里面的一个属性类记录状态
+    if (!currentComponent.touchendExist) {
+      currentComponent.addEventListener('touchend', touchEndFunction);
+      currentComponent.touchendExist = true;
+    }
+  });
+
+}
+
+export const destoryTouchEvent = () => {
+  // 在React中使用DOM原声事件的时候一定要在组件卸载的时候手动移除，以免内存泄漏  
+  document.body.removeEventListener('touchmove');
+}
+
+export default Slider;
